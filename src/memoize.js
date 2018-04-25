39120c04d79695e122797177cb70b2538f8813da
Dmitriy Konovalov <Dmitrii_Konovalov1@epam.com>
2018-04-25 13:51:54 +0300

    add package-lock.json, create function()

--- src/memoize.js
+++ src/memoize.js
@@ -1,3 +1,14 @@
-export function memoize() {
-    // TODO
+export function memoize(f) {
+    if (typeof f !== 'function') {
+        return null;
+    }
+    const cache = new Map();
+    return function mem(...line) {
+        const find = ((line.length !== 0) ? JSON.stringify(line) : 0);
+        if (cache.has(find)) {
+            return cache.get(find);
+        }
+        cache.set(find, f.apply(this, line));
+        return cache.get(find);
+    };
 }
